d7ae54907c19cabb4e81b5bfe2f37ceeb9a6208a
godcong <jumbycc@163.com>
2019-03-26 15:24:44 +0800

    update seed

--- seed.go
+++ seed.go
@@ -7,29 +7,30 @@ type VideoSource struct {
 	Role      []interface{} `json:"role"`      //主演
 	Sharpness string        `json:"sharpness"` //清晰度
 	Publish   string        `json:"publish"`   //发布日期
-}
+} //上传视频JSON配置
 
 type VideoLink struct {
 	Hash string `json:"hash"`
 	Name string `json:"name"`
 	Size uint64 `json:"size"`
 	Type int    `json:"type"`
-}
+} //视频IPFS地址信息
 
-type VideoLinkInfo struct {
-	Sharpness string    `json:"sharpness"` //清晰度
-	Sliced    bool      `json:"sliced"`    //切片
-	Type      string    `json:"type"`      //目录，文件
-	Index     string    `json:"index"`     //文件
-	VideoLink VideoLink `json:"video_link_list"`
-}
+type VideoGroup struct {
+	Sharpness string       `json:"sharpness"`  //清晰度
+	Sliced    bool         `json:"sliced"`     //切片
+	VideoLink VideoLink    `json:"video_link"` //视频源
+	PlayList  []*VideoLink `json:"play_list"`  //具体信息
+} //整套片源
 
 type Video struct {
-	Bangumi string        `json:"bangumi"` //番号
-	Poster  string        `json:"poster"`  //海报
-	Role    []interface{} `json:"role"`    //主演
-	Publish string        `json:"publish"` //发布日期
+	VideoInfo      VideoInfo     `json:"video_info"`       //基本信息
+	VideoGroupList []*VideoGroup `json:"video_group_list"` //多套片源
 }
 
 type VideoInfo struct {
-}
+	Bangumi string   `json:"bangumi"` //番号
+	Poster  string   `json:"poster"`  //海报
+	Role    []string `json:"role"`    //主演
+	Publish string   `json:"publish"` //发布日期
+} //视频信息

3d1e82fee1d9d5e0ec57980b0f8dbfd42db1e98b
godcong <jumbycc@163.com>
2019-04-03 17:59:57 +0800

    update seed

--- seed.go
+++ seed.go
@@ -20,7 +20,7 @@ type HLS struct {
 
 // VideoSource ...
 type VideoSource struct {
-	Bangu        string    `json:"bangu"`                 //番号
+	Bangou       string    `json:"bangou"`                //番号
 	Type         string    `json:"type"`                  //类型：film，FanDrama
 	Output       string    `json:"output"`                //输出：3D，2D
 	VR           string    `json:"vr"`                    //VR格式：左右，上下，平面
@@ -62,24 +62,24 @@ type VideoGroup struct {
 
 // VideoInfo ...
 type VideoInfo struct {
-	Bangu        string   `json:"bangu"`               //番号
-	Type         string   `json:"type"`                //类型：film，FanDrama
-	Output       string   `json:"output"`              //输出：3D，2D
-	VR           string   `json:"vr"`                  //VR格式：左右，上下，平面
-	Thumb        string   `json:"thumb"`               //缩略图
-	Intro        string   `json:"intro"`               //简介
-	Alias        []string `json:"alias"`               //别名，片名
-	Language     string   `json:"language"`            //语言
-	Caption      string   `json:"caption"`             //字幕
-	Poster       string   `json:"poster"`              //海报
-	Role         []string `json:"role"`                //主演
-	Director     string   `json:"director"`            //导演
-	Season       string   `json:"season"`              //季
-	Episode      string   `bson:"episode"`             //集数
-	TotalEpisode string   `bson:"total_episode"`       //总集数
-	Sharpness    string   `json:"sharpness,omitempty"` //清晰度
-	Group        string   `json:"group"`               //分组
-	Publish      string   `json:"publish,omitempty"`   //发布日期
+	Bangou       string   `json:"bangou"`        //番号
+	Type         string   `json:"type"`          //类型：film，FanDrama
+	Output       string   `json:"output"`        //输出：3D，2D
+	VR           string   `json:"vr"`            //VR格式：左右，上下，平面
+	Thumb        string   `json:"thumb"`         //缩略图
+	Intro        string   `json:"intro"`         //简介
+	Alias        []string `json:"alias"`         //别名，片名
+	Language     string   `json:"language"`      //语言
+	Caption      string   `json:"caption"`       //字幕
+	Poster       string   `json:"poster"`        //海报
+	Role         []string `json:"role"`          //主演
+	Director     string   `json:"director"`      //导演
+	Season       string   `json:"season"`        //季
+	Episode      string   `bson:"episode"`       //集数
+	TotalEpisode string   `bson:"total_episode"` //总集数
+	Sharpness    string   `json:"sharpness"`     //清晰度
+	Group        string   `json:"group"`         //分组
+	Publish      string   `json:"publish"`       //发布日期
 }
 
 // SourceInfo ...

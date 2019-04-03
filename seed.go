548c06681e024bcb9007b303b797c5895dc11ffc
godcong <jumbycc@163.com>
2019-04-03 17:53:14 +0800

    update seed

--- seed.go
+++ seed.go
@@ -58,18 +58,28 @@ type VideoGroup struct {
 	Sliced    bool      `json:"sliced"`           //切片
 	HLS       *HLS      `json:"hls,omitempty"`    //切片信息
 	Object    []*Object `json:"object,omitempty"` //视频信息
-	//PlayList  []*VideoLink `json:"play_list,omitempty"`  //具体信息
 }
 
 // VideoInfo ...
 type VideoInfo struct {
-	Bangu    string   `json:"bangu"`    //番号
-	Alias    []string `json:"alias"`    //别名，片名
-	Language string   `json:"language"` //语言
-	Caption  string   `json:"caption"`  //字幕
-	Poster   string   `json:"poster"`   //海报
-	Role     []string `json:"role"`     //主演
-	Publish  string   `json:"publish"`  //发布日期
+	Bangu        string   `json:"bangu"`               //番号
+	Type         string   `json:"type"`                //类型：film，FanDrama
+	Output       string   `json:"output"`              //输出：3D，2D
+	VR           string   `json:"vr"`                  //VR格式：左右，上下，平面
+	Thumb        string   `json:"thumb"`               //缩略图
+	Intro        string   `json:"intro"`               //简介
+	Alias        []string `json:"alias"`               //别名，片名
+	Language     string   `json:"language"`            //语言
+	Caption      string   `json:"caption"`             //字幕
+	Poster       string   `json:"poster"`              //海报
+	Role         []string `json:"role"`                //主演
+	Director     string   `json:"director"`            //导演
+	Season       string   `json:"season"`              //季
+	Episode      string   `bson:"episode"`             //集数
+	TotalEpisode string   `bson:"total_episode"`       //总集数
+	Sharpness    string   `json:"sharpness,omitempty"` //清晰度
+	Group        string   `json:"group"`               //分组
+	Publish      string   `json:"publish,omitempty"`   //发布日期
 }
 
 // SourceInfo ...

74aa9d864826af75314cf92cbc710d1d11e7875d
godcong <jumbycc@163.com>
2019-04-04 15:24:13 +0800

    update seed

--- ffmpeg.go
+++ ffmpeg.go
@@ -22,7 +22,8 @@ func SplitVideo(ctx context.Context, source *VideoSource, file string) (e error)
 	command := cmd.NewFFMPEG()
 	command.Split(path).
 		HlsSegmentFilename(source.HLS.SegmentFile).Output(source.HLS.M3U8).
-		Input(file).Ignore().CodecAudio(cmd.String("aac")).CodecVideo(cmd.String("libx264")).
+		Input(file).Ignore().
+		CodecAudio(cmd.String("aac")).CodecVideo(cmd.String("libx264")).
 		BitStreamFiltersVideo("h264_mp4toannexb").Format("hls").HlsTime("10").
 		HlsListSize("0")
 	info := make(chan string)

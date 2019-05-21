5cce12abd22ddc08cda80726fe2cd3b8ce5e88ff
godcong <jumbycc@163.com>
2019-05-21 15:40:20 +0800

    update seed

--- transfer.go
+++ transfer.go
@@ -21,32 +21,8 @@ func Transfer() (e error) {
 	return nil
 }
 
-// TransferMysql ...
-func TransferMysql(eng *xorm.Engine, limit int) (e error) {
-	i, e := model.DB().Count(&model.Video{})
-	if e != nil || i <= 0 {
-		return e
-	}
-	if limit == 0 {
-		limit = 10
-	}
-	for x := 0; x <= int(i); x += limit {
-		var videos []*model.Video
-		if e = model.DB().Limit(limit, x).Find(&videos); e != nil {
-			return xerrors.Errorf("transfer error with:%d,%+v", x, e)
-		}
-		for _, v := range videos {
-			log.Info("get:", v.Bangumi)
-		}
-		insert, e := eng.Insert(videos)
-		log.Info(insert, e)
-
-	}
-	return nil
-}
-
-// TransferOther ...
-func TransferOther(name string, limit int) (e error) {
+// TransferTo ...
+func TransferTo(eng *xorm.Engine, limit int) (e error) {
 	i, e := model.DB().Count(&model.Video{})
 	if e != nil || i <= 0 {
 		return e

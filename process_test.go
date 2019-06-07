0ea48eb52f5fa9e1b15bc2f3a0c39caf0a3fbe18
godcong <jumbycc@163.com>
2019-06-08 03:09:13 +0800

    update seed

--- process_test.go
+++ process_test.go
@@ -9,7 +9,7 @@ import (
 // TestGetFiles ...
 func TestGetFiles(t *testing.T) {
 	Rest()
-	process := NewProcess("D:\\video")
+	process := NewProcessSeeder("D:\\video")
 	process.Start()
 	process.Wait()
 }

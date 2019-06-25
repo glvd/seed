8084890b0606c86fff910980eb8a2b925a5371bd
godcong <jumbycc@163.com>
2019-06-25 16:04:03 +0800

    update seed

--- transfer_test.go
+++ transfer_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // TestTransfer ...
 func TestTransfer(t *testing.T) {
-	seed := NewSeed(Transfer("D:\\videoall\\video2.json", InfoFlagBSON, TransferStatusUpdate), Pin(), Update(UpdateStatusAdd))
+	seed := NewSeed(Transfer("D:\\videoall\\video2.json", InfoFlagBSON, TransferStatusUpdate), Pin(PinStatusAll), Update(UpdateStatusAdd))
 	seed.Workspace = "D:\\videoall"
 	seed.Start()
 	seed.Wait()

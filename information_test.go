86748cffc4f291493d7e60178a7887a97bda50bd
godcong <jumbycc@163.com>
2019-06-26 15:38:50 +0800

    update seed

--- information_test.go
+++ information_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // TestInformation ...
 func TestInformation(t *testing.T) {
-	seed := NewSeed(Information("D:\\videoall\\video2.json", InfoFlagBSON), DatabaseOption("sqlite3", "test.db"))
+	seed := NewSeed(Information("D:\\videoall\\vtest.json", InfoFlagBSON), DatabaseOption("sqlite3", "t1.db"))
 	seed.Workspace = "D:\\videoall"
 	seed.AfterInit(SyncDatabase())
 	seed.Start()

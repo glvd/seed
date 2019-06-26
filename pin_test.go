3d26f3618c7cf935f6ba7b89f07b4b2d33745224
godcong <jumbycc@gmail.com>
2019-06-26 15:56:14 +0800

    update seed

--- pin_test.go
+++ pin_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 // TestPin ...
 func TestPin(t *testing.T) {
-	seed := NewSeed(Information("D:\\videoall\\video2.json", InfoFlagBSON), Pin(PinStatusAll))
-	seed.Workspace = "D:\\videoall"
+	seed := NewSeed(DatabaseOption("sqlite3", "t1.db"), Pin(PinStatusAll))
+	//seed.Workspace = "D:\\videoall"
 	//seed.AfterInit()
 	seed.Start()
 

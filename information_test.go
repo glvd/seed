3db3adbc10e510f461d1c64af0b19a1a6dad3d05
godcong <jumbycc@163.com>
2019-07-11 18:33:52 +0800

    update seed

--- information_test.go
+++ information_test.go
@@ -11,3 +11,14 @@ func TestInformation(t *testing.T) {
 
 	seed.Wait()
 }
+
+// TestInformation2 ...
+func TestInformation2(t *testing.T) {
+	seed := NewSeed(Information("D:\\videoall\\video.json", InfoFlagBSON), DatabaseOption("sqlite3", "t1.db"), MoveInfo("D:\\videoall\\picSuccess"))
+	seed.Workspace = "D:\\videoall"
+	seed.AfterInit(SyncDatabase())
+	seed.MaxLimit = 2
+	seed.Start()
+
+	seed.Wait()
+}

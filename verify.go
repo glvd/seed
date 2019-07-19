6f29837e9e0e05da298d0b79078a8903274a1967
godcong <jumbycc@163.com>
2019-07-19 14:10:04 +0800

    update seed

--- verify.go
+++ verify.go
@@ -1,19 +1,60 @@
 package seed
 
-import "os"
+import (
+	cmd "github.com/godcong/go-ffmpeg-cmd"
+	"os"
+	"path/filepath"
+)
 
-// Verify ...
-func Verify(source *VideoSource) (e error) {
-	_, e = os.Stat(source.PosterPath)
-	if os.IsNotExist(e) {
-		return e
+type Verify struct {
+	path string
+}
+
+func (v *Verify) getFiles(ws string) (files []string) {
+	info, e := os.Stat(ws)
+	if e != nil {
+		return nil
 	}
-
-	for _, value := range source.Files {
-		_, e := os.Stat(value)
-		if os.IsNotExist(e) {
-			return e
+	if info.IsDir() {
+		file, e := os.Open(ws)
+		if e != nil {
+			return nil
 		}
+		defer file.Close()
+		names, e := file.Readdirnames(-1)
+		if e != nil {
+			return nil
+		}
+		var fullPath string
+		for _, name := range names {
+			fullPath = filepath.Join(ws, name)
+			tmp := v.getFiles(fullPath)
+			if tmp != nil {
+				files = append(files, tmp...)
+			}
+		}
+		return files
 	}
-	return nil
+	return append(files, ws)
+}
+
+func NewVerify(path string) *Verify {
+	return &Verify{
+		path: path,
+	}
+}
+
+func (v *Verify) Check() (sfs map[string]*cmd.StreamFormat) {
+	files := v.getFiles(v.path)
+	sfs = make(map[string]*cmd.StreamFormat, len(files))
+	for _, f := range files {
+		format, e := cmd.FFProbeStreamFormat(f)
+		if e != nil {
+			log.Error(e)
+			continue
+		}
+		_, file := filepath.Split(f)
+		sfs[file] = format
+	}
+	return sfs
 }

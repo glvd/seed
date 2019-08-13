7d194e79252ffbc98846d1076cc2230b1e66bd6a
godcong <jumbycc@163.com>
2019-08-13 17:50:19 +0800

    update seed

--- check_test.go
+++ check_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // TestCheck ...
 func TestCheck(t *testing.T) {
-	seed := NewSeed(DatabaseOption("sqlite3", "test.db"), Check("recursive"))
+	seed := NewSeed(DatabaseOption("sqlite3", "test.db"), Check(CheckTypeArg("recursive")))
 	seed.AfterInit(SyncDatabase())
 	seed.Start()
 	seed.Wait()

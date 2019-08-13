362f30d59aefddfe7a1d46d8ad59998d29d69615
godcong <jumbycc@163.com>
2019-08-13 18:27:38 +0800

    update seed

--- check_test.go
+++ check_test.go
@@ -1,6 +1,13 @@
 package seed
 
-import "testing"
+import (
+	"math/rand"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/godcong/go-trait"
+)
 
 // TestCheck ...
 func TestCheck(t *testing.T) {
@@ -8,4 +15,65 @@ func TestCheck(t *testing.T) {
 	seed.AfterInit(SyncDatabase())
 	seed.Start()
 	seed.Wait()
+}
+
+// TestSliceBenchmark ...
+func BenchmarkSliceBenchmark(b *testing.B) {
+	var vals []interface{}
+	for i := 0; i < 250000; i++ {
+		vals = append(vals, trait.GenerateRandomString(32))
+		vals = append(vals, rand.Intn(time.Now().Nanosecond()))
+	}
+	b.StartTimer()
+	count := 0
+	for i := 0; i < 100; i++ {
+		if checkSlice(trait.GenerateRandomString(3), vals...) {
+			count++
+		}
+	}
+	b.StopTimer()
+	b.Log(count)
+}
+
+// TestSliceBenchmark ...
+func BenchmarkArrayBenchmark(b *testing.B) {
+	var vals []interface{}
+	for i := 0; i < 250000; i++ {
+		vals = append(vals, trait.GenerateRandomString(32))
+		vals = append(vals, rand.Intn(time.Now().Nanosecond()))
+	}
+	b.StartTimer()
+	count := 0
+	for i := 0; i < 100; i++ {
+		if checkArray(trait.GenerateRandomString(3), vals) {
+			count++
+		}
+	}
+	b.StopTimer()
+	b.Log(count)
+}
+
+func checkSlice(s string, v ...interface{}) bool {
+	for i := range v {
+		if vv, b := (v[i]).(string); b {
+			if strings.Index(vv, s) > 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func checkArray(s string, v []interface{}) bool {
+	if v == nil {
+		return false
+	}
+	for i := range v {
+		if vv, b := (v[i]).(string); b {
+			if strings.Index(vv, s) > 0 {
+				return true
+			}
+		}
+	}
+	return false
 }

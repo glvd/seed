3633425e043dc1f1d9c9e1bf1f44c9487228233a
godcong <jumbycc@163.com>
2019-08-14 13:19:12 +0800

    update seed

--- pin.go
+++ pin.go
@@ -280,13 +280,14 @@ func (p *pin) Run(ctx context.Context) {
 			}
 		}
 	case PinStatusSync:
-		i, e := model.DB().Where("machine_id like %?%", p.from).Count(model.Pin{})
+		s := model.DB().Where("machine_id like %?%", p.from)
+		i, e := s.Clone().Count(model.Pin{})
 		if e != nil {
 			log.Error(e)
 			return
 		}
 		for start := 0; start < int(i); start += 50 {
-			pins, e := model.AllPin(model.DB().Where("machine_id like %?%", p.from), 50, start)
+			pins, e := model.AllPin(s.Clone(), 50, start)
 			if e != nil {
 				log.Error(e)
 				return

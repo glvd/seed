0cef972e526b42ec26bbcf640030a3d7216b77d1
godcong <jumbycc@163.com>
2019-08-16 16:38:16 +0800

    update seed

--- api.go
+++ api.go
@@ -1,1 +1,16 @@
 package seed
+
+import httpapi "github.com/ipfs/go-ipfs-http-client"
+
+type API struct {
+	api *httpapi.HttpApi
+	cb  chan APICallbackAble
+}
+
+func NewAPI() *API {
+	return new(API)
+}
+
+type APICallbackAble interface {
+	Callback(api *httpapi.HttpApi)
+}

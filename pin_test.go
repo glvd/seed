1d3a9b15cca51e425ab6d8fefebec77e925b66c0
godcong <jumbycc@163.com>
2019-08-18 00:04:53 +0800

    update seed

--- pin_test.go
+++ pin_test.go
@@ -1,10 +1,24 @@
 package seed_test
 
-import "testing"
+import (
+	"github.com/glvd/seed"
+	"github.com/glvd/seed/model"
+	"log"
+	"testing"
+)
 
 // TestPin ...
 func TestPin(t *testing.T) {
-	seed := NewSeed(DatabaseOption("sqlite3", "data.db"), Pin(PinStatusArg(PinStatusAll)))
+	db, e := model.InitSQLite3("data.db")
+	if e != nil {
+		log.Fatal(e)
+	}
+	sdb := seed.NewDatabase(db, seed.DatabaseShowSQLArg())
+
+	seed := seed.NewSeed(sdb)
+
+	seed.Register()
+
 	//seed.Workspace = "D:\\videoall"
 	//seed.AfterInit()
 	seed.Start()
--- process_test.go
+++ process_test.go
@@ -3,45 +3,42 @@ package seed_test
 import (
 	"testing"
 
-	"github.com/glvd/seed/model"
-
 	_ "github.com/mattn/go-sqlite3"
 )
 
 // TestGetFiles ...
 func TestGetFiles(t *testing.T) {
 
-	unfin, _ := model.FindUnfinished(nil, "d0384c15ca0862e3d558e9d610219a4bc9433f74")
+	//unfin, _ := model.FindUnfinished(nil, "d0384c15ca0862e3d558e9d610219a4bc9433f74")
 
-	seed := NewSeed(IgnoreOption("d:\\videos\\tmp"), Process("D:\\videoall\\videos"), UnfinishedOption(unfin), Pin(PinStatusArg(PinStatusAll)))
-	seed.Workspace = "d:\\videos\\tmp"
+	//seed := NewSeed(IgnoreOption("d:\\videos\\tmp"), Process("D:\\videoall\\videos"), UnfinishedOption(unfin), Pin(PinStatusArg(PinStatusAll)))
+	//seed.Workspace = "d:\\videos\\tmp"
 	//seed.ProcessPath ="d:\\videos"
-	seed.Start()
-	seed.Wait()
+	//seed.Start()
+	//seed.Wait()
 
 }
 
 // TestProcess ...
 func TestProcess(t *testing.T) {
-	seed := NewSeed(Process("D:\\videoall\\videos"), SkipConvertOption(), DatabaseOption("sqlite3", "t1.db"))
-	seed.Workspace = "d:\\videos\\tmp"
+	//seed := NewSeed(Process("D:\\videoall\\videos"), SkipConvertOption(), DatabaseOption("sqlite3", "t1.db"))
+	//seed.Workspace = "d:\\videos\\tmp"
 	//seed.ProcessPath ="d:\\videos"
-	seed.AfterInit(SyncDatabase())
-	seed.Start()
-	seed.Wait()
+	//seed.AfterInit(SyncDatabase())
+	//seed.Start()
+	//seed.Wait()
 }
 
 // TestName ...
 func TestName(t *testing.T) {
-	t.Log(onlyNo("file-09-B.name"))
-	t.Log(onlyNo("file-09B.name"))
-	t.Log(onlyNo("file-001R"))
-	t.Log(onlyNo(".file"))
-	t.Log(onlyNo("."))
-	t.Log(onlyNo(""))
-	t.Log(NumberIndex("file-09"))
-	t.Log(NumberIndex("file-09-C"))
-	t.Log(NumberIndex("file-09-B"))
-	t.Log(NumberIndex("file-09-A"))
-
+	//t.Log(onlyNo("file-09-B.name"))
+	//t.Log(onlyNo("file-09B.name"))
+	//t.Log(onlyNo("file-001R"))
+	//t.Log(onlyNo(".file"))
+	//t.Log(onlyNo("."))
+	//t.Log(onlyNo(""))
+	//t.Log(NumberIndex("file-09"))
+	//t.Log(NumberIndex("file-09-C"))
+	//t.Log(NumberIndex("file-09-B"))
+	//t.Log(NumberIndex("file-09-A"))
 }

729628e0476ccdd2e2af49f8b5110e03294d0470
godcong <jumbycc@163.com>
2019-08-19 13:40:25 +0800

    update seed

--- seed.go
+++ seed.go
@@ -292,47 +292,6 @@ type Extend struct {
 }
 
 // VideoSource ...
-type VideoSource struct {
-	Bangumi      string    `json:"bangumi"`       //番号 no
-	SourceHash   string    `json:"source_hash"`   //原片hash
-	Type         string    `json:"type"`          //类型：film，FanDrama
-	Format       string    `json:"format"`        //输出：3D，2D
-	VR           string    `json:"vr"`            //VR格式：左右，上下，平面
-	Thumb        string    `json:"thumb"`         //缩略图
-	Intro        string    `json:"intro"`         //简介 title
-	Alias        []string  `json:"alias"`         //别名，片名
-	VideoEncode  string    `json:"video_encode"`  //视频编码
-	AudioEncode  string    `json:"audio_encode"`  //音频编码
-	Files        []string  `json:"files"`         //存放路径
-	HashFiles    []string  `json:"hash_files"`    //已上传Hash
-	CheckFiles   []string  `json:"check_files"`   //Unfinished checksum
-	Slice        bool      `json:"sliceAdd"`      //是否HLS切片
-	Encrypt      bool      `json:"encrypt"`       //加密
-	Key          string    `json:"key"`           //秘钥
-	M3U8         string    `json:"m3u8"`          //M3U8名
-	SegmentFile  string    `json:"segment_file"`  //ts切片名
-	PosterPath   string    `json:"poster_path"`   //海报路径
-	Poster       string    `json:"poster"`        //海报HASH
-	ExtendList   []*Extend `json:"extend_list"`   //扩展信息
-	Role         []string  `json:"role"`          //角色列表 stars
-	Director     string    `json:"director"`      //导演
-	Systematics  string    `json:"systematics"`   //分级
-	Season       string    `json:"season"`        //季
-	Episode      string    `json:"episode"`       //集数
-	TotalEpisode string    `json:"total_episode"` //总集数
-	Sharpness    string    `json:"sharpness"`     //清晰度
-	Publish      string    `json:"publish"`       //发行日
-	Date         string    `json:"date"`          //发行日
-	Length       string    `json:"length"`        //片长
-	Producer     string    `json:"producer"`      //制片商
-	Series       string    `json:"series"`        //系列
-	Tags         []string  `json:"tags"`          //标签
-	Publisher    string    `json:"publisher"`     //发行商
-	Language     string    `json:"language"`      //语言
-	Caption      string    `json:"caption"`       //字幕
-	MagnetLinks  []string  `json:"magnet_links"`  //磁链
-	Uncensored   bool      `json:"uncensored"`    //有码,无码
-}
 
 // Hash ...
 func Hash(v interface{}) string {

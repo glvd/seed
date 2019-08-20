ff8f8c1cc9e3f1365feded0078cc6cbdf78cbba2
godcong <jumbycc@163.com>
2019-08-20 14:17:07 +0800

    update seed

--- check.go
+++ check.go
@@ -6,6 +6,7 @@ import (
 	"github.com/glvd/seed/model"
 	"github.com/go-xorm/xorm"
 	httpapi "github.com/ipfs/go-ipfs-http-client"
+	iface "github.com/ipfs/interface-go-ipfs-core"
 	"github.com/ipfs/interface-go-ipfs-core/options"
 )
 
@@ -36,55 +37,64 @@ func (c *Check) Option(seed *Seed) {
 // Run ...
 func (c *Check) Run(ctx context.Context) {
 	log.Info("Check running")
-	switch c.checkType {
-	case CheckTypePin:
-		c.API.PushRun(func(api *API, api2 *httpapi.HttpApi) error {
-			pins, e := api2.Pin().Ls(ctx, func(settings *options.PinLsSettings) error {
-				settings.Type = c.Type
-				return nil
-			})
-			if e != nil {
-				return e
-			}
-			return pins
-		})
-		for _, path := range pins {
-			log.With("path", path.Path()).Info("pinned")
-			p := &model.Pin{
-				PinHash: model.PinHash(path.Path()),
-				PeerID:  []string{c.myID.ID},
-				VideoID: "",
-			}
-			c.Database.PushCallback(func(database *Database, eng *xorm.Engine) (e error) {
-				return model.UpdatePinVideoID(eng.NewSession(), p)
-			})
-		}
-	case CheckTypeUnpin:
-		pins, e := c.api.Pin().Ls(context.Background(), func(settings *options.PinLsSettings) error {
+	cPin := make(chan iface.Pin)
+	c.API.PushRun(func(api *API, api2 *httpapi.HttpApi) (e error) {
+		defer func() {
+			cPin <- nil
+		}()
+		pins, e := api2.Pin().Ls(ctx, func(settings *options.PinLsSettings) error {
 			settings.Type = c.Type
 			return nil
 		})
 		if e != nil {
-			log.Error(e)
-			return
+			return e
 		}
-		unf := make(chan *model.Unfinished)
+		for _, p := range pins {
+			cPin <- p
+		}
 
-		go func(u chan<- *model.Unfinished) {
-			defer func() {
-				u <- nil
-			}()
-			s := model.DB().NewSession()
-			if len(c.skipType) > 0 {
-				s.NotIn("type", c.skipType...)
-			}
-			i, e := s.Clone().Count(model.Unfinished{})
-			if e != nil {
-				log.Error(e)
+		return nil
+	})
+	switch c.checkType {
+	case CheckTypePin:
+	PinList:
+		for {
+			select {
+			case <-ctx.Done():
 				return
+			case pin := <-cPin:
+				if pin == nil {
+					break PinList
+				}
+				log.With("path", pin.Path()).Info("pinned")
+				p := &model.Pin{
+					PinHash: model.PinHash(pin.Path()),
+					PeerID:  []string{c.myID.ID},
+					VideoID: "",
+				}
+				c.Database.PushCallback(func(database *Database, eng *xorm.Engine) (e error) {
+					return model.UpdatePinVideoID(eng.NewSession(), p)
+				})
+
+			}
+		}
+
+	case CheckTypeUnpin:
+		unf := make(chan *model.Unfinished)
+		c.Database.PushCallback(func(database *Database, eng *xorm.Engine) (e error) {
+			defer func() {
+				unf <- nil
+			}()
+			session := eng.NewSession()
+			if len(c.skipType) > 0 {
+				session.NotIn("type", c.skipType...)
+			}
+			i, e := session.Clone().Count(model.Unfinished{})
+			if e != nil {
+				return e
 			}
 			for start := 0; start < int(i); start += 50 {
-				unfins, e := model.AllUnfinished(s.Clone(), 50, start)
+				unfins, e := model.AllUnfinished(session.Clone(), 50, start)
 				if e != nil {
 					log.Error(e)
 					return
@@ -92,30 +102,36 @@ func (c *Check) Run(ctx context.Context) {
 
 				log.Infof("pin(%d)", len(*unfins))
 				for i := range *unfins {
-					u <- (*unfins)[i]
+					unf <- (*unfins)[i]
 				}
 			}
+			return nil
+		})
 
-		}(unf)
 		//var retUnf []*model.Unfinished
-	CheckEnd:
+		pins := make(map[string][]byte)
+	PinList2:
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case pin := <-cPin:
+				if pin == nil {
+					break PinList2
+				}
+				pins[model.PinHash(pin.Path())] = nil
+			}
+		}
+	CheckList2:
 		for {
 			select {
 			case u := <-unf:
 				if u == nil {
-					break CheckEnd
+					break CheckList2
 				}
-				pinned := false
-				for _, path := range pins {
-					if u.Hash == model.PinHash(path.Path()) {
-						pinned = true
-					}
-					//retUnf = append(retUnf, u)
-				}
-				if !pinned {
+				if _, b := pins[u.Hash]; b {
 					log.With("hash", u.Hash, "relate", u.Relate, "type", u.Type).Info("unpin")
 				}
-
 			}
 		}
 

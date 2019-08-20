559a2b7b1d338914ffb1e6edbed1e9ee7a939dda
godcong <jumbycc@163.com>
2019-08-20 18:19:15 +0800

    update seed

---
+++ slice.go
@@ -0,0 +1,31 @@
+package seed
+
+import (
+	"context"
+
+	cmd "github.com/godcong/go-ffmpeg-cmd"
+)
+
+// Slice ...
+type Slice struct {
+	Seed  *Seed
+	Scale int64
+	File  chan string
+}
+
+// Run ...
+func (s *Slice) Run(context.Context) {
+
+	var sa *cmd.SplitArgs
+
+}
+
+// BeforeRun ...
+func (s *Slice) BeforeRun(seed *Seed) {
+	panic("implement me")
+}
+
+// AfterRun ...
+func (s *Slice) AfterRun(seed *Seed) {
+	panic("implement me")
+}

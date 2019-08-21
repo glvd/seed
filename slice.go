e0b396674c94925ea2f29969691079b08fb3b5ee
godcong <jumbycc@163.com>
2019-08-21 18:25:31 +0800

    update seed

--- slice.go
+++ slice.go
@@ -2,26 +2,63 @@ package seed
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 )
 
 // Slice ...
 type Slice struct {
-	Seed  *seed
+	Seeder
 	Scale int64
-	File  chan string
+	Path  string
 }
 
 // Run ...
 func (s *Slice) Run(context.Context) {
+	log.Info("slice running")
+	files := GetFiles(s.Path)
+
+	for _, file := range files {
+
+	}
 
 }
 
 // BeforeRun ...
-func (s *Slice) BeforeRun(seed *seed) {
-	panic("implement me")
+func (s *Slice) BeforeRun(seed Seeder) {
+	s.Seeder = seed
+
 }
 
 // AfterRun ...
-func (s *Slice) AfterRun(seed *seed) {
-	panic("implement me")
+func (s *Slice) AfterRun(seed Seeder) {
+}
+
+// GetFiles ...
+func GetFiles(p string) (files []string) {
+	info, e := os.Stat(p)
+	if e != nil {
+		return nil
+	}
+	if info.IsDir() {
+		file, e := os.Open(p)
+		if e != nil {
+			return nil
+		}
+		defer file.Close()
+		names, e := file.Readdirnames(-1)
+		if e != nil {
+			return nil
+		}
+		var fullPath string
+		for _, name := range names {
+			fullPath = filepath.Join(p, name)
+			tmp := GetFiles(fullPath)
+			if tmp != nil {
+				files = append(files, tmp...)
+			}
+		}
+		return files
+	}
+	return append(files, p)
 }

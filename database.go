ff2ea64eb388fcef5701a7a3fd39eda3ff025a05
godcong <jumbycc@163.com>
2019-08-22 12:58:59 +0800

    update seed

--- database.go
+++ database.go
@@ -185,5 +185,6 @@ func DatabaseShowExecTimeArg() DatabaseArgs {
 func databaseOption(db *Database) Options {
 	return func(seed Seeder) {
 		seed.SetNormalThread(StepperDatabase, db)
+		seed.SetNormalThread(StepperRDatabase, db)
 	}
 }

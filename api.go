b1706ff14d9fd0c0fdda9b9163a92c9c17ceafa7
godcong <jumbycc@163.com>
2019-08-23 14:25:30 +0800

    update seed

--- api.go
+++ api.go
@@ -2,11 +2,9 @@ package seed
 
 import (
 	"context"
-	"sync"
 	"time"
 
 	httpapi "github.com/ipfs/go-ipfs-http-client"
-	"github.com/ipfs/interface-go-ipfs-core/path"
 	"github.com/multiformats/go-multiaddr"
 	"go.uber.org/atomic"
 	"golang.org/x/xerrors"
@@ -77,9 +75,6 @@ func NewAPI(path string) *API {
 	return a
 }
 
-// CallbackFunc ...
-type CallbackFunc func(*API, *httpapi.HttpApi) error
-
 // PushCallback ...
 func (api *API) pushAPICallback(cb interface{}) (e error) {
 	if v, b := cb.(APICaller); b {
@@ -126,69 +121,6 @@ type PeerID struct {
 	PublicKey       string   `json:"PublicKey"`
 }
 
-// APIPeerID ...
-func APIPeerID(seed Seeder) *PeerID {
-	pid := new(apiPeerID)
-	pid.done = make(chan bool)
-	e := seed.PushTo(StepperAPI, pid)
-	if e != nil {
-		return nil
-	}
-	d := <-pid.done
-	if d {
-		return pid.id
-	}
-	return nil
-}
-
-type apiPeerID struct {
-	id   *PeerID
-	done chan bool
-}
-
-// Done ...
-func (p *apiPeerID) Done() {
-	p.done <- true
-}
-
-// Failed ...
-func (p *apiPeerID) Failed() {
-	p.done <- false
-}
-
-// OnDone ...
-func (p *apiPeerID) OnDone() *PeerID {
-	d := <-p.done
-	if d {
-		return p.id
-	}
-
-	return nil
-}
-
-// Callback ...
-func (p *apiPeerID) Callback(api *API, api2 *httpapi.HttpApi) (e error) {
-	p.id = new(PeerID)
-	e = api2.Request("id").Exec(context.Background(), p.id)
-	if e != nil {
-		return e
-	}
-	return nil
-}
-
-// APIPin ...
-func APIPin(seed Seeder, hash string) (e error) {
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	e = seed.PushTo(StepperAPI, func(api *API, api2 *httpapi.HttpApi) error {
-		defer wg.Done()
-		e = api2.Pin().Add(context.Background(), path.New(hash))
-		return e
-	})
-	wg.Wait()
-	return e
-}
-
 // APICallback ...
 func APICallback(v interface{}, cb APICallbackFunc) (Stepper, APICaller) {
 	return StepperAPI, &apiCall{
@@ -205,11 +137,4 @@ type apiCall struct {
 // Callback ...
 func (a *apiCall) Call(api *API, api2 *httpapi.HttpApi) error {
 	return a.cb(api, api2, a.v)
-}
-
-// APIOption ...
-func APIOption(s string) Options {
-	return func(seed Seeder) {
-		seed.SetThread(StepperAPI, NewAPI(s))
-	}
 }

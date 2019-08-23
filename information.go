0302d79c9717bfb1d9096dace9206c47124f4f13
godcong <jumbycc@163.com>
2019-08-23 13:22:43 +0800

    update seed

--- information.go
+++ information.go
@@ -39,6 +39,12 @@ type Information struct {
 	ProcList     []string
 	Start        int
 	vcb          chan InformationVideoCallback
+	state        *atomic.Int32
+}
+
+// State ...
+func (info *Information) State() State {
+	return State(info.state.Load())
 }
 
 // Push ...
@@ -173,6 +179,10 @@ func checkFileNotExist(path string) bool {
 // Run ...
 func (info *Information) Run(ctx context.Context) {
 	log.Info("information running")
+	info.state.Store(int32(StateRunning))
+	defer func() {
+		info.state.Store(int32(StateStop))
+	}()
 	var vs []*VideoSource
 	isDefault := true
 	switch info.InfoType {

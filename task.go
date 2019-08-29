a7157b070376a061adb96d6e3e8ea9d0a1767f22
godcong <jumbycc@163.com>
2019-08-29 16:35:50 +0800

    update seed

--- task.go
+++ task.go
@@ -2,11 +2,24 @@ package seed
 
 // TaskProcessor ...
 type TaskProcessor interface {
-	Call(process *Process)
+	Call(process *Process) error
 }
 
 // Task ...
 type Task struct {
 	Name string
 	TaskProcessor
+}
+
+// TaskCall ...
+func TaskCall(seeder Seeder, task *Task) error {
+	return seeder.PushTo(StepperProcess, task)
+}
+
+// InformationTask ...
+func InformationTask(task *Information) *Task {
+	return &Task{
+		Name:          "information",
+		TaskProcessor: task,
+	}
 }

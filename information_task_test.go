2f56ce039f628cf47d707ea5a4ae23e7cdca9b13
godcong <jumbycc@163.com>
2019-08-29 17:26:53 +0800

    update seed

--- information_task_test.go
+++ information_task_test.go
@@ -1,54 +1,39 @@
 package seed_test
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/glvd/seed"
-	"github.com/glvd/seed/model"
 )
 
 // TestInformation ...
 func TestInformation(t *testing.T) {
-	info := seed.NewInformation()
-	info.ResourcePath = "D:\\videoall\\images"
-	info.Path = "D:\\videoall\\video3.json"
-	info.InfoType = seed.InfoTypeBSON
-	s := seed.NewSeed(info)
-	s.Start()
 
-	e := seed.SplitCall(info, 10000)
-	if e != nil {
-		t.Error(e)
-	}
-	s.Wait()
 }
 
 // TestInformation2 ...
 func TestInformation2(t *testing.T) {
-	info := seed.NewInformation()
-	info.ResourcePath = "D:\\videoall"
-	info.Path = "D:\\videoall\\video3.json"
-	info.InfoType = seed.InfoTypeBSON
-	sdb := seed.NewDatabase(model.MustDatabase(model.InitSQLite3("test.db")))
-	sdb.RegisterSync(model.Video{}, model.Pin{}, model.Unfinished{})
-
-	api := seed.NewAPI("/ip4/127.0.0.1/tcp/5001")
-
-	s := seed.NewSeed(info, sdb, api)
-
-	s.Start()
-	fmt.Println("waiting end")
-	//e := seed.SplitCall(info, 10000)
-	//if e != nil {
-	//	t.Error(e)
+	//info := seed.NewInformation()
+	//info.ResourcePath = "D:\\videoall"
+	//info.Path = "D:\\videoall\\video3.json"
+	//info.InfoType = seed.InfoTypeBSON
+	//sdb := seed.NewDatabase(model.MustDatabase(model.InitSQLite3("test.db")))
+	//sdb.RegisterSync(model.Video{}, model.Pin{}, model.Unfinished{})
+	//
+	//api := seed.NewAPI("/ip4/127.0.0.1/tcp/5001")
+	//
+	//s := seed.NewSeed(info, sdb, api)
+	//
+	//s.Start()
+	//fmt.Println("waiting end")
+	////e := seed.SplitCall(info, 10000)
+	////if e != nil {
+	////	t.Error(e)
+	////}
+	//if err := s.PushTo(seed.InformationCall(info.InfoType, info.Path)); err != nil {
+	//	t.Error(err)
 	//}
-	if err := s.PushTo(seed.InformationCall(info.InfoType, info.Path)); err != nil {
-		t.Error(err)
-	}
-	s.Wait()
-	fmt.Println("waiting db end")
-	sdb.Done()
-	fmt.Println("db end")
+	//s.Wait()
+	//fmt.Println("waiting db end")
+	//sdb.Done()
+	//fmt.Println("db end")
 
 }
--- slice_test.go
+++ slice_test.go
@@ -14,16 +14,16 @@ func TestNewSlice(t *testing.T) {
 	sli := seed.NewSlice()
 	sli.SliceOutput = "d:\\temp"
 
-	info := seed.NewInformation()
-	info.ResourcePath = "D:\\videoall"
-	info.Path = "D:\\videoall\\video4.json"
-	info.InfoType = seed.InfoTypeBSON
+	//info := seed.NewInformation()
+	//info.ResourcePath = "D:\\videoall"
+	//info.Path = "D:\\videoall\\video4.json"
+	//info.InfoType = seed.InfoTypeBSON
 	sdb := seed.NewDatabase(model.MustDatabase(model.InitSQLite3("test.db")))
 	sdb.RegisterSync(model.Video{}, model.Pin{}, model.Unfinished{})
 
 	api := seed.NewAPI("/ip4/127.0.0.1/tcp/5001")
 
-	s := seed.NewSeed(info, sdb, api, sli)
+	s := seed.NewSeed(sdb, api, sli)
 	s.Start()
 	fmt.Println("waiting end")
 	e := s.PushTo(seed.SliceCall("D:\\videoall\\videos\\MIAA-087.mp4", &model.Unfinished{}, func(s *seed.Slice, sa *cmd.SplitArgs, v interface{}) (e error) {

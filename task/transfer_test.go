09acb2b57eb4e478a91e3680ead0d3a61d89056d
godcong <jumbycc@163.com>
2019-09-09 14:08:29 +0800

    update seed

--- task/transfer_test.go
+++ task/transfer_test.go
@@ -1,11 +1,30 @@
 package task_test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/glvd/seed"
+	"github.com/glvd/seed/model"
+	"github.com/glvd/seed/task"
+)
 
 // TestTransfer ...
 func TestTransfer(t *testing.T) {
-	//seed := NewSeed(DatabaseOption("sqlite3", "d:\\cs.db"), Transfer("D:\\workspace\\goproject\\seed\\old\\seed.db", InfoFlagSQLite, TransferStatusFromOld))
-	//seed.Workspace = "D:\\videoall"
-	//seed.Start()
-	//seed.Wait()
+	dbt := task.NewDBTransfer(model.MustDatabase(model.InitSQLite3("cs.db")))
+	//jst := task.NewJSONTransfer()
+	sdb := seed.NewDatabase(model.MustDatabase(model.InitSQLite3("test.db")))
+	sdb.RegisterSync(model.Video{}, model.Pin{}, model.Unfinished{})
+	//
+	api := seed.NewAPI("/ip4/127.0.0.1/tcp/5001")
+	proc := seed.NewProcess()
+
+	s := seed.NewSeed(sdb, api, proc)
+	//
+	s.Start()
+	fmt.Println("waiting end")
+
+	s.AddTasker(dbt)
+
+	s.Wait()
 }
